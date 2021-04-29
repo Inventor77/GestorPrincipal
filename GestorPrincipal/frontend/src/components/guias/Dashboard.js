145cf0d26fdd5c9b92ecf2bb7c685683e06b0e4b
Shreedutt <shreedutt77@gmail.com>
2021-04-29 08:41:12 +0530

    Form.js, Dashboard.js, Header.js, Guias.js have been added to the repo

---
+++ GestorPrincipal/frontend/src/components/guias/Dashboard.js
@@ -0,0 +1,13 @@
+import React,{Fragment} from 'react'
+import Form from './Form'
+import Guias from './Guias'
+
+export default function Dashboard() {
+    return (
+        <Fragment>
+            <Form />
+            <Guias/>
+        </Fragment>
+            
+    )
+}
